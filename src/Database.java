7ed9c70dfb7b6df675a95b7b91dcc12f8107bbae
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-19 08:37:05 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/Database.java changed at Fri Feb 19 08:36:22 EST 2016

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,4 @@
-/**
+/** 
  * 
  */
 
@@ -6,8 +6,53 @@
  * @author prestonlattimer
  *
  */
-public class Database {
+public class Database<K extends Comparable<K>, E> {
 
 	
+	/**
+	 * creates the skipList for the database
+	 */
+	private SkipList list;
 	
+	public Database()
+	{
+		list = new SkipList();
+	}
+	
+	public void insert()
+	{
+		/** not yet implemented
+		 * 
+		 */
+	}
+	
+	/**
+	 * not yet implemented
+	 */
+	public void remove()
+	{
+		// not implemented
+	}
+	/**
+	 * not implemented
+	 */
+	public void duplicates()
+	{
+		//not yet implemented
+	}
+	
+	/**
+	 * not implemented
+	 */
+	public void dump(){};
+	
+	/**
+	 * not implemented
+	 */
+	public void search(){};
+	
+	/**
+	 * not implemented
+	 */
+	public void regionsearch(){};
 }
