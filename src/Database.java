2a0cf58305823fb4b281a4e77f2e7d835df15f2d
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-19 08:24:05 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/DatabaseTest.java changed at Fri Feb 19 08:23:57 EST 2016

---
+++ src/Database.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author prestonlattimer
+ *
+ */
+public class Database {
+
+}
---
+++ src/DatabaseTest.java
@@ -0,0 +1,25 @@
+import student.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author prestonlattimer
+ *
+ */
+public class DatabaseTest extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+
+	public void setUp() throws Exception {
+	}
+
+
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
