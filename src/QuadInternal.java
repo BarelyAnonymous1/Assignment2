8630200da3d3c55b32d10d07111cbadfbcb2515d
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-23 17:17:48 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/QuadInternal.java changed at Tue Feb 23 17:17:42 EST 2016

--- src/QuadInternal.java
+++ src/QuadInternal.java
@@ -6,41 +6,66 @@
  * @author prestonlattimer
  *
  */
-public class QuadInternal<K extends Comparable<K>, E> implements QuadNode {
+public class QuadInternal<K extends Comparable<K>, E> implements QuadNode<K, E> {
 
+	private int depth;
+	private LinkedList<K, E> data;
 	/**
 	 * 
 	 */
 	public QuadInternal() {
-		// TODO Auto-generated constructor stub
+		depth = 0;
+		data = null;
 	}
 
 	@Override
-	public void traverse(QuadNode root) {
-		// TODO Auto-generated method stub
+	public void traverse(QuadNode<K, E> root) {
+		if (root == null) return;
+		int i = 0;
+		
 		
 	}
 
 	@Override
-	public void dump(QuadNode root) {
-		// TODO Auto-generated method stub
-		
-<<<<<<< HEAD
-=======
-	}
-
-	@Override
-	public LinkedNode<K, E> find(QuadNode root) {
+	public LinkedNode<K, E> remove(QuadNode<K, E> root) {
 		// TODO Auto-generated method stub
 		return null;
->>>>>>> branch 'master' of https://github.com/BarelyAnonymous1/Assignment2.git
 	}
 
 	@Override
-	public void insert(QuadNode root) {
+	public LinkedList<K, E> getElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setElement(LinkedList<K, E> list) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public void dump(QuadNode<K, E> root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public LinkedNode<K, E> find(QuadNode<K, E> root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(QuadNode<K, E> root) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void getData()
+	{
+		data.dump();
+		return;
+	}
 
 }
