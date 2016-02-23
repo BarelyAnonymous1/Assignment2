331b78801e404a787a2629dded7dcd30f8ec8487
Jonathan <jondef95@vt.edu>
2016-02-23 16:42:45 -0500

    skeleton for linkedlist class.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,34 +2,35 @@
 public class LinkedList<E>
 {
     private LinkedNode<E> head;
+    private int           size;
+
+    public LinkedList()
+    {
+        head = null;
+        size = 0;
+    }
 
     public LinkedList(LinkedNode<E> startHead)
     {
         head = startHead;
+        size = 1;
     }
 
     public void insert(LinkedNode<E> newNode)
     {
         if (head == null)
+        {
             head = newNode;
+            size++;
+        } 
         else
         {
             LinkedNode<E> curr = head;
             while (curr.getNext() != null)
                 curr.setNext(curr.getNext());
             curr.setNext(newNode);
+            size++;
         }
     }
 
-}
-
-public void QuadTreeDump(QuadNode root)
-{
-    if (root == null){}
-    else
-    {
-        root.dump();
-        QuadTreeDump(root.left());
-        QuadTreeDump(root.right());
-    }
 }
