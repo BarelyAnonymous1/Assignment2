a2bb7e9ea927cf091b0d3f0e75a4a9e366dd9e18
Jonathan <jondef95@vt.edu>
2016-02-23 20:39:45 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/Database.java changed at Tue Feb 23 20:39:44 EST 2016

--- src/Database.java
+++ src/Database.java
@@ -6,75 +6,84 @@
  * @author prestonlattimer
  *
  */
-public class Database<K extends Comparable<K>, E> {
+public class Database<K extends Comparable<K>, E>
+{
 
-	
-	/**
-	 * creates the skipList for the database
-	 */
-	private SkipList<K, E> list;
-	private QuadTree<K, E> tree;
-	
-	public Database()
-	{
-		list = new SkipList<K, E>();
-		tree = new QuadTree<K, E>();
-	}
-	
-	/**
-	 * 
-	 * @param pair is the value to be inserted
-	 */
-	public void insert(KVPair<K, E> pair)
-	{
-		/** not yet implemented
-		 * 
-		 */
-	}
-	
-	/**
-	 * not yet implemented
-	 * @param key is the key to be searched
-	 */
-	public E removeKey(K key)
-	{
-		return null;
-	}
-	/**
-	 * 
-	 * @param val is the value to be found
-	 */
-	public E removeValue(E val)
-	{
-		return null;
-	}
-	/**
-	 * not implemented
-	 */
-	public void duplicates()
-	{
-		//not yet implemented
-	}
-	
-	/**
-	 * not implemented
-	 */
-	public void dump()
-	{
-		list.dump();
-		tree.dump();
-	}
-	
-	/**
-	 * not implemented
-	 */
-	public SkipNode<K, E> search(K key)
-	{
-		return null;
-	}
-	
-	/**
-	 * not implemented
-	 */
-	public void regionSearch(E region){};
+    /**
+     * creates the skipList for the database
+     */
+    private SkipList<K, E> list;
+    private QuadTree<K, E> tree;
+
+    public Database()
+    {
+        list = new SkipList<K, E>();
+        tree = new QuadTree<K, E>();
+    }
+
+    /**
+     * 
+     * @param pair
+     *            is the value to be inserted
+     */
+    public void insert(KVPair<K, E> pair)
+    {
+        /**
+         * not yet implemented
+         * 
+         */
+    }
+
+    /**
+     * not yet implemented
+     * 
+     * @param key
+     *            is the key to be searched
+     */
+    public E removeKey(K key)
+    {
+        return null;
+    }
+
+    /**
+     * 
+     * @param val
+     *            is the value to be found
+     */
+    public E removeValue(E val)
+    {
+        return null;
+    }
+
+    /**
+     * not implemented
+     */
+    public void duplicates()
+    {
+        // not yet implemented
+    }
+
+    /**
+     * not implemented
+     */
+    public void dump()
+    {
+        list.dump();
+        tree.dump();
+    }
+
+    /**
+     * not implemented
+     */
+    public SkipNode<K, E> search(K key)
+    {
+        return null;
+    }
+
+    /**
+     * not implemented
+     */
+    public void regionSearch(E region)
+    {
+    };
 }
