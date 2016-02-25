c64e1344c311342934aa51e428b1d2f4ea7ed1c0
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-25 14:30:22 -0500

    tester

--- src/QuadLeaf.java
+++ src/QuadLeaf.java
@@ -1,55 +1,49 @@
-//
-//public class QuadLeaf implements QuadNode<K, E>
-//{
-//
-//
-//    @Override
-//    public LinkedNode<K, E> remove(QuadNode<K, E> root)
-//    {
-//        // TODO Auto-generated method stub
-//        return null;
-//    }
-//
-//    @Override
-//    public LinkedList<K, E> getElement()
-//    {
-//        // TODO Auto-generated method stub
-//        return null;
-//    }
-//
-//    @Override
-//    public void setElement(LinkedList<K, E> list)
-//    {
-//        // TODO Auto-generated method stub
-//
-//    }
-//
-//    @Override
-//    public void dump(QuadNode<K, E> root)
-//    {
-//        // TODO Auto-generated method stub
-//
-//    }
-//
-//    @Override
-//    public LinkedNode<K, E> find(QuadNode<K, E> root)
-//    {
-//        // TODO Auto-generated method stub
-//        return null;
-//    }
-//
-//    @Override
-//    public void insert(QuadNode<K, E> root)
-//    {
-//        // TODO Auto-generated method stub
-//
-//    }
-//
-//    @Override
-//    public void getData()
-//    {
-//        // TODO Auto-generated method stub
-//
-//    }
-//
-//}
+
+public class QuadLeaf<K comparable > implements QuadNode<K, E>
+{
+
+	@Override
+	public LinkedNode<K, E> remove(QuadNode<K, E> root, KVPair<K, E> pair) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedList<K, E> getElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setElement(LinkedList<K, E> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dump(QuadNode<K, E> root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public LinkedNode<K, E> find(QuadNode<K, E> root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(QuadNode<K, E> root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
