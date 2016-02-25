ba8ba5506954f737339779038b310681b13062ca
Jonathan <jondef95@vt.edu>
2016-02-25 15:09:05 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/QuadLeaf.java changed at Thu Feb 25 15:09:05 EST 2016
    toString

--- src/QuadLeaf.java
+++ src/QuadLeaf.java
@@ -95,6 +95,7 @@ public class QuadLeaf implements QuadNode
                 output += curr.getData().toString() + "\n";                
                 curr = curr.getNext();
             }
+            return output;
         }
     }
 }
