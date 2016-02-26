53f6fe136805498b87ee6d6fc28d5e7a9e4f34e6
jondef95@vt.edu <jondef95@github.com>
2016-02-25 23:33:22 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/QuadTree.java changed at Thu Feb 25 23:33:18 EST 2016

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -6,7 +6,7 @@
  */
 public class QuadTree
 {
-    private QuadNode root;
+    private QuadNode        root;
     private static QuadLeaf flyLeaf;
 
     public QuadTree()
@@ -14,17 +14,10 @@ public class QuadTree
         flyLeaf = new QuadLeaf();
         root = flyLeaf;
     }
-    
+
     public void insert(Point newPoint)
     {
-        if (root.getData() == null)
-        {
-            root = new QuadLeaf(newPoint);
-        }
-        else
-        {
-            root.insert(root, 0, 0, 1024, newPoint);
-        }
+        root.insert(root, 0, 0, 1024, newPoint);
     }
 
     /**
@@ -34,14 +27,15 @@ public class QuadTree
     public void dump()
     {
         System.out.println("QuadTree dump:");
-        System.out.println(root.dump(root, 0, 0, 1024, 0) + " quadtree nodes printed");
+        System.out.println(root.dump(root, 0, 0, 1024, 0)
+                + " quadtree nodes printed");
     }
-    
+
     public QuadNode getRoot()
     {
         return root;
     }
-    
+
     public static QuadLeaf getFlyLeaf()
     {
         return flyLeaf;
