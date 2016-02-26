68fc42ef11a775e5f2317f3d4aa0a14b992b723f
jondef95@vt.edu <jondef95@github.com>
2016-02-26 00:52:16 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/QuadInternal.java changed at Fri Feb 26 00:52:15 EST 2016

--- src/QuadInternal.java
+++ src/QuadInternal.java
@@ -42,7 +42,7 @@ public class QuadInternal implements QuadNode
         }
         printer += "Node at " + x + ", " + y + ", " + width + ": Internal  ";
         System.out.println(printer + depth);
-        int newDepth = depth++;
+        int newDepth = 14;
         return 1 + root.dump(northWest, x, y, width / 2, newDepth)
                 + root.dump(northEast, width / 2, y, width / 2, newDepth)
                 + root.dump(southWest, x, width / 2, width / 2, newDepth)
