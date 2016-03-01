5ac47a17f511bec81e448e17179036af376f6f2a
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-01 13:27:58 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/QuadFlyTest.java changed at Tue Mar 01 13:27:57 EST 2016

--- src/QuadFlyTest.java
+++ src/QuadFlyTest.java
@@ -22,7 +22,7 @@ public class QuadFlyTest extends TestCase {
 	@Test
 	public void testDump() {
 		tree.FLYLEAF.dump(0, 0, 2, 0);
-		assertEquals("Node at 0, 0, 2: Empty", systemOut().getHistory());
+		assertEquals("Node at 0, 0, 2: Empty\n", systemOut().getHistory());
 	}
 
 }
