be6dc1f29ba6a1acc0935ff553aa02752b2a6a35
Jonathan <jondef95@vt.edu>
2016-03-01 14:55:31 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/QuadLeaf.java changed at Tue Mar 01 14:55:31 EST 2016

--- src/QuadLeaf.java
+++ src/QuadLeaf.java
@@ -14,6 +14,10 @@ public class QuadLeaf implements QuadNode
     private LinkedList data;
 
     
+    public QuadLeaf()
+    {
+        data = new LinkedList();
+    }
     /**
      * standard constructor for the leaf; when a leaf is created, it will have
      * data stored into it, so a new Point is inserted and a new LinkedList is
