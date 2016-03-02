55585b5d27b7d7a4f64bb71f3a39a4c244b8035b
jondef95@vt.edu <jondef95@github.com>
2016-03-01 21:15:36 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/QuadInternal.java changed at Tue Mar 01 21:15:36 EST 2016

--- src/QuadInternal.java
+++ src/QuadInternal.java
@@ -182,7 +182,6 @@ public class QuadInternal implements QuadNode
     public QuadNode adjustTree(int x, int y, int width)
     {
         int numUniques = removeEmpty();
-        System.out.println(numUniques);
         if (numUniques == 0)
         {
             return QuadTree.FLYLEAF;
