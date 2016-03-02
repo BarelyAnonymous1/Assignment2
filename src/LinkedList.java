6d68806a5814b83a828b1cfe782c571ed6d576fc
jondef95@vt.edu <jondef95@github.com>
2016-03-01 21:51:02 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/LinkedList.java changed at Tue Mar 01 21:51:01 EST 2016

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -154,6 +154,8 @@ public class LinkedList
         }
         else
         {
+            System.out.println("Size: " + size);
+            System.out.println("numUniques: " + numUnique);
             LinkedNode curr = head;
             int newSize = 1;
             int newUnique = 1;
