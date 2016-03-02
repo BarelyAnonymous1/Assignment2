d4d9fc4f10c0e5c5fdcaa832dda0c1db5df143c4
jondef95@vt.edu <jondef95@github.com>
2016-03-01 23:27:30 -0500

    File /C:/Users/Jonathan/git/Assignment2/src/QuadTree.java changed at Tue Mar 01 23:27:30 EST 2016

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -49,7 +49,7 @@ public class QuadTree
     public Point remove(Point findPoint, boolean byName)
     {
         Point output =  root.remove(0, 0, 1024, findPoint, byName);
-        root.adjustTree(0, 0, 1024);
+        root = root.adjustTree(0, 0, 1024);
         return output;
     }
 }
