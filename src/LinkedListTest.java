c43c4023ea531eb1d2d5d9baac5ac752d3f0a6f6
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-03 13:55:35 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/LinkedListTest.java changed at Thu Mar 03 13:55:34 EST 2016

--- src/LinkedListTest.java
+++ src/LinkedListTest.java
@@ -53,7 +53,7 @@ public class LinkedListTest extends TestCase {
 	{
 		//list.remove(null, true);
 		//list.remove(null, false);
-		list.remove(point1, true);
-		list.remove(point1, false);
+		//list.remove(point1, true);
+		//list.remove(point1, false);
 	}
 }
