38d53668eec7cd133f4cc8126a1cdc52a68c08a8
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-03 13:09:16 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/QuadTreeTest.java changed at Thu Mar 03 13:09:16 EST 2016

--- src/QuadTreeTest.java
+++ src/QuadTreeTest.java
@@ -84,6 +84,7 @@ public class QuadTreeTest extends TestCase {
 		tree.insert(point1);
 		tree.insert(point2);
 		tree.insert(point3);
+		tree.insert(point1);
 		tree.dump();
 		tree.remove(point1, false);
 		tree.dump();
