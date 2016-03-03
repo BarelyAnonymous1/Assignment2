9e2e86bc21d69a349804d84f29da5805d9fdf3ea
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-03 13:54:31 -0500

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/LinkedList.java changed at Thu Mar 03 13:54:30 EST 2016

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -207,6 +207,10 @@ public class LinkedList
      */
     public Point remove(Point searchPoint, boolean byName)
     {
+    	if (searchPoint == null)
+    	{
+    		return null;
+    	}
         LinkedNode curr = head;
         if (curr.getData().equals(searchPoint))
         {
