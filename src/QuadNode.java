b5ef95bdd979c9ea58ab203fa5b72e379295acab
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-14 14:27:35 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/QuadNode.java changed at Mon Mar 14 14:25:59 EDT 2016

--- src/QuadNode.java
+++ src/QuadNode.java
@@ -42,6 +42,13 @@ public interface QuadNode
      */
     public QuadNode insert(int x, int y, int width, Point newPoint);
 
+    /**
+     * fixes the tree after removal of a point
+     * @param x the x point to look for
+     * @param y the y point to look for
+     * @param width the width of adjustment
+     * @return the node that was adjusted
+     */
     public QuadNode adjustTree(int x, int y, int width);
     /**
      * getter for the data stored in the node
@@ -49,10 +56,23 @@ public interface QuadNode
      * @return data in the node
      */
     public LinkedList getData();
-    
+    /**
+     * looks for duplicate points
+     */
     public void duplicates();
-    
+    /**
+     * removes a point
+     * @param x is the x point to look for
+     * @param y the y point to look for
+     * @param width the width of search
+     * @param removePoint what point to remove
+     * @param byName whether the name is necessary for SkipList
+     * @return
+     */
     public Point remove(int x, int y, int width, Point removePoint, boolean byName);
     
+    /**
+     * @return whether a node is unique
+     */
     public int getUnique();
 }
