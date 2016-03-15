811622682e9f3ca2eba89b727b19a4e004acdf2d
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-15 14:28:30 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/RectangleTest.java changed at Tue Mar 15 14:27:33 EDT 2016

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -282,8 +282,11 @@ public class RectangleTest extends TestCase
         
         Rectangle rect1 = new Rectangle("rect1", 0, 0, 1023, 1023);
         Rectangle rect2 = new Rectangle("rect2", 2, 2, 1000, 1000);
+        Rectangle rect3 = new Rectangle("rect3", 1, 1, 1, 1);
         assertTrue(rect1.contains(point1));
         assertFalse(rect2.contains(point1));
-        assertFalse(rect2.contains(point2));
+        assertFalse(rect3.contains(point2));
+        assertFalse(rect2.contains(point3));
+        
     }
 }
