fd864da84e7432c23c4d8504bd8e053edc819637
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-15 14:05:42 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment2/src/QuadInternalTest.java changed at Tue Mar 15 14:05:42 EDT 2016

---
+++ src/QuadInternalTest.java
@@ -0,0 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import student.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author platt, jondef95
+ * @version 1
+ *
+ */
+public class QuadInternalTest extends TestCase {
+
+    QuadInternal node1;
+    QuadInternal node2;
+    QuadInternal node3;
+    /**
+     * sets up the class
+     */
+    public void setUp() {
+        node1 = new QuadInternal();
+        node2 = new QuadInternal();
+        node3 = new QuadInternal();
+    }
+
+    /**
+     * tests the remove class
+     */
+    public void testRemove()
+    {
+        Point searchPoint = new Point("a", 54, 10);
+        assertNull(node1.remove(0, 4, 20, searchPoint, true));
+    }
+}
